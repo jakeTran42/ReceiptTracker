463f43d5de10640954015cc4eb9cf49acd287020
jakeTran42 <jake.tranehs@gmail.com>
2017-07-11 13:32:09 -0700

    Login Flow

---
+++ Pods/Target Support Files/Pods-Receipt Tracker/Pods-Receipt Tracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Receipt_Tracker : NSObject
+@end
+@implementation PodsDummy_Pods_Receipt_Tracker
+@end
---
+++ Receipt Tracker/Controllers/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Receipt Tracker
+//
+//  Created by Jake Tran on 7/11/17.
+//  Copyright © 2017 Jake Tran. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Additional Set up after loading view
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        //Dispose of any resource that can be recreated
+    }
+}
